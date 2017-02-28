1216c282bfb776ffbc61103ac2ca13caa46c75ce
hawkab <hwakaba@ya.ru>
2017-02-28 21:31:34 +0300

    refactoring

--- runThreads.py
+++ runThreads.py
@@ -5,11 +5,11 @@ import multiprocessing , sys
 from runTest import *
 
 count = 10 #default
+
 if len ( sys.argv )>1:
 	count = int ( sys.argv[1])
 
 inputs = range ( count )
 
 num_cores = multiprocessing.cpu_count()
-
-results = Parallel ( n_jobs = num_cores )( delayed ( runRandomTest )( i ) for i in inputs )
+results = Parallel ( n_jobs = num_cores )( delayed ( runTest )( i ) for i in inputs )
